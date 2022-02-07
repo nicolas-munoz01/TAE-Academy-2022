b5d853118e7eb61331cb7cefdbbb5b4c20480540
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-02-07 12:30:30 -0500

    Adding multiplication and division operations to Calculator class

--- Git Basic/src/Calculator.java
+++ Git Basic/src/Calculator.java
@@ -1,10 +1,10 @@
 public class Calculator {
 
-    public int sumOperation(int firstNumber, int secondNumber){
-        return firstNumber + secondNumber;
+    public double multiplicationOperation(double firstNumber, double secondNumber){
+        return  firstNumber * secondNumber;
     }
 
-    public int substractOperation(int firstNumber, int secondNumber){
-        return firstNumber - secondNumber;
+    public double divisionOperation(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
     }
 }
