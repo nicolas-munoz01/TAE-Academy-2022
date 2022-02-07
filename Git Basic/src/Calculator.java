cbf5f702fe9de500264381eac643a210e55ccf46
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-02-07 12:23:33 -0500

    Adding sum and substract operation to Calculator class

---
+++ Git Basic/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public int sumOperation(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public int substractOperation(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+}
