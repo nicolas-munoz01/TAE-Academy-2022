4a057ea05cc22f7e8d99ca5c7dd434ae1818a937
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-02-07 12:36:56 -0500

    Merging base_calculator to advanced_calculator, adding sum and substract operations

--- Git Basic/src/Calculator.java
+++ Git Basic/src/Calculator.java
@@ -1,5 +1,13 @@
 public class Calculator {
 
+    public int sumOperation(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public int substractOperation(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+
     public double multiplicationOperation(double firstNumber, double secondNumber){
         return  firstNumber * secondNumber;
     }
